33863bc1afd4ac664c8515f0c46e0bd5fe4d3869
Ubuntu <mars.leezm@gmail.com>
2014-08-28 09:27:54 +0000

    Refine script

--- script/startNodes.sh
+++ script/startNodes.sh
@@ -7,9 +7,12 @@ AllNodes="10.20.0.77 10.20.0.78 10.20.0.79"
 Stop="sudo floppystore/rel/floppy/bin/floppy stop" 
 Remove="sudo rm -rf floppystore/rel/floppy/data/*"
 Start="sudo floppystore/rel/floppy/bin/floppy start"
+Join="sudo floppystore/rel/floppy/bin/floppy-admin cluster join floppy@$First"
+Plan="sudo floppystore/rel/floppy/bin/floppy-admin cluster plan"
+Commit="sudo floppystore/rel/floppy/bin/floppy-admin cluster commit"
 ./script/command_to_all.sh "$AllNodes" "$Stop" 
 ./script/command_to_all.sh "$AllNodes" "$Remove" 
 ./script/command_to_all.sh "$AllNodes" "$Start" 
-./script/command_to_all.sh "$Others" 'sudo floppystore/rel/floppy/bin/floppy-admin cluster join "floppy@10.20.0.77"'
-./script/command_to_all.sh "$First" 'sudo floppystore/rel/floppy/bin/floppy-admin cluster plan'
-./script/command_to_all.sh "$First" 'sudo floppystore/rel/floppy/bin/floppy-admin cluster commit'
+./script/command_to_all.sh "$Others" "$Join"
+./script/command_to_all.sh "$First" "$Plan"
+./script/command_to_all.sh "$First" "$Commit"
