fe1ac86170193771e038e2dfc4430464e413ef4f
root <root@ip-172-31-16-173.ec2.internal>
2015-02-26 15:23:26 +0000

    Add pull to master

--- script/configIPs.sh
+++ script/configIPs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-AllNodes="172.31.54.217 172.31.54.218 172.31.54.219 172.31.54.220 172.31.54.221"
+#AllNodes="172.31.54.217 172.31.54.218 172.31.54.219 172.31.54.220 172.31.54.221"
+AllNodes="172.31.23.49 172.31.30.71"
 File1="./antidote/rel/vars.config"
 File2="./antidote/rel/files/app.config"
 Command1="sudo sed -i 's/172.31.52.119/localhost/g' $File1"
 Command2="sudo sed -i 's/127.0.0.1/localhost/g' $File2"
-Command3="cd ./antidote/ && sudo git fetch && sudo git checkout evaluation && sudo make rel"
+#Command3="cd ./antidote/ && sudo git fetch && sudo git checkout evaluation && sudo make rel"
+Command3="cd ./antidote/ && sudo git fetch && sudo git checkout master && sudo git pull && sudo make rel"
 ./script/command_to_all.sh "$AllNodes" "$Command1"	
 ./script/command_to_all.sh "$AllNodes" "$Command2"	
 ./script/command_to_all.sh "$AllNodes" "$Command3"	
--- script/runMultipleTests.sh
+++ script/runMultipleTests.sh
@@ -3,21 +3,21 @@
 #FirstNode="172.31.52.119"
 #AllNodes="172.31.52.119 172.31.48.15 172.31.59.7 172.31.59.197 172.31.59.198"
 #RestNodes="172.31.52.119 172.31.48.15 172.31.59.7 172.31.59.197 172.31.59.198"
-FirstNode="172.31.54.217"
-AllNodes="172.31.54.217 172.31.54.218 172.31.54.219 172.31.54.220 172.31.54.221"
-RestNodes="172.31.54.217 172.31.54.218 172.31.54.219 172.31.54.220 172.31.54.221"
+FirstNode="172.31.23.49"
+AllNodes="172.31.23.49 172.31.30.71"
+RestNodes="172.31.23.49 172.31.30.71"
 #AllNodes="172.31.52.119 10.20.0.78 10.20.0.79"
 #RestNodes="172.31.52.119 10.20.0.78"
 Cookie="antidote"
 Mode="pb"
-./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 1
+#./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 1
 ./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 2 
 #./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 3 
-./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 4 
+#/script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 4 
 
 
 Mode="erl"
-./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 1
+#./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 1
 ./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 2 
 #./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 3 
-./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 4 
+#./script/runMultiDCBenchmark.sh "$AllNodes" "$RestNodes" antidote 1 $Mode 4 
