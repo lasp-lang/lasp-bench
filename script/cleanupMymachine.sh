a6f064001970bd27337c90489a77ac2f94841286
Zhongmiao Li <mars.leezm@gmail.com>
2015-03-24 22:09:37 +0000

    Clean up my machine

---
+++ script/cleanupMymachine.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Fetching latest update for basho_bench"
+sudo git stash
+sudo git pull origin benchmark_cluster
+sudo rm -rf deps/riak_pb
+sudo rm -rf deps/antidote_pb
+sudo make
+
+echo "Fetching latest update for antidote"
+cd ../antidote
+sudo git stash
+sudo git pull origin evaluation
+sudo make
+
+
