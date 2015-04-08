4b41e89d756f0dbcbf03fe415e04d341b5280025
root <root@ip-172-31-58-181.ec2.internal>
2015-04-08 22:04:38 +0000

    Add config scripts

--- script/runMultipleTests.sh
+++ script/runMultipleTests.sh
@@ -2,15 +2,5 @@
 
 AllNodes=`cat script/allnodes`
 Mode="pb"
-./script/adjustTime.sh
-sleep 120
-./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 1 4 1 $Mode 
-sleep 120
-./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 1 4 1 $Mode 
-
-
 ./script/preciseTime.sh
-sleep 120
-./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 1 4 1 $Mode 
-sleep 120
-./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 1 4 1 $Mode 
+./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 4 1 1 $Mode 
