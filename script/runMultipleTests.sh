7eed6948417b6f380d7395fca15c02e4815167c9
root <root@ip-172-31-32-87.ec2.internal>
2015-04-14 01:01:33 +0000

    Different branch

--- script/runMultipleTests.sh
+++ script/runMultipleTests.sh
@@ -3,4 +3,4 @@
 AllNodes=`cat script/allnodes`
 Mode="pb"
 ./script/preciseTime.sh
-./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 4 1 1 $Mode 
+./script/runMultiDCBenchmark.sh "$AllNodes"  antidote 1 1 1 $Mode 
