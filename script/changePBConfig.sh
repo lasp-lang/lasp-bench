f2c1566cea133d3a2643190c9892a26706849997
root <root@ip-172-31-33-87.ec2.internal>
2015-05-05 12:47:25 +0000

    Adjust thread number

--- script/changePBConfig.sh
+++ script/changePBConfig.sh
@@ -4,12 +4,17 @@ AllNodes=$1
 Cookie=$2
 File="./"$3
 sudo sed -i '/antidote_pb_ips/d' $File 
+sudo sed -i '/concurrent/d' $File
+PerNodeNum=20
+Thread=0
 BenchConfig="{antidote_pb_ips, ["
 for Node in $AllNodes
 do
     Node=\'$Node\',
-	BenchConfig=$BenchConfig$Node
+    BenchConfig=$BenchConfig$Node
+    Thread=$((Thread+PerNodeNum))
 done
 BenchConfig=${BenchConfig::-1}"]}."
 echo $BenchConfig
 sudo echo "$BenchConfig" >> $File
+sudo sed -i "5i {concurrent, $Thread}." $File
--- script/cleanupMymachine.sh
+++ script/cleanupMymachine.sh
@@ -7,10 +7,10 @@ sudo rm -rf deps/riak_pb
 sudo rm -rf deps/antidote_pb
 sudo make
 
-echo "Fetching latest update for antidote"
-cd ../antidote
-sudo git stash
-sudo git pull origin evaluation
-sudo make
+#echo "Fetching latest update for antidote"
+#cd ../antidote
+#sudo git stash
+#sudo git pull origin evaluation
+#sudo make
 
 
