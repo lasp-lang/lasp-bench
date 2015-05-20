831af8e79d8542eb249e2ed06150cfab8b1fe3e9
Zhongmiao Li <mars.leezm@gmail.com>
2015-05-20 19:03:42 +0100

    Fix error in connectin script

--- script/connectNodesToOtherDCs.sh
+++ script/connectNodesToOtherDCs.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+
 ExternalIPs=$2
 
 Nodes=`head -$1 ./script/allnodes`
 First=`head -1 ./script/allnodes`
 
-LocalExIP=`./script/command_to_all.sh $First curl "http://169.254.169.254/latest/meta-data/public-ipv4"`
+LocalExIP=`ssh -i key ubuntu@$First "curl \"http://169.254.169.254/latest/meta-data/public-ipv4\""`
 
-for IP in "${ExternalIPs[@]}"
+Array=($ExternalIPs)
+for IP in "${Array[@]}"
 do
     if [ "$IP" = "$LocalExIP" ]
     then
+        echo here
         NewExtIPs=$NewExtIps" "$IP
-    fi 
+    fi
 done
 
-echo "$Nodes"
-echo "$NewExtIP"
 
-erl -pa script -setcookie antidote -name setup@localhost -run connectDCs connect  $3 $Nodes  $NewExtIP
+erl -noshell -pa script -setcookie antidote -name setup@localhost -run connectDCs connect  $1 $Nodes  $NewExtIPs
