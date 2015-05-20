ba6cc63693b689cc2d8b2bff7bf97350c4704ee1
Zhongmiao Li <mars.leezm@gmail.com>
2015-05-20 17:53:24 +0100

    Fix bug in change partitions

--- script/changePartition.sh
+++ script/changePartition.sh
@@ -2,7 +2,7 @@
 
 Nodes=`cat script/allnodes`
 File1="./antidote/rel/antidote/etc/app.config"
-Command1="sudo sed -i '7d' $File1 &&  sudo sed -i '7i \'$'\n' $File"
+Command1="sudo sed -i '7d' $File1 &&  sudo sed -i '7i \'$'\n' $File1"
 Command2="sudo sed -i '7i  {ring_creation_size, $1},' $File1"
 Command3="sudo rm -r ./antidote/rel/antidote/data/*"
 
