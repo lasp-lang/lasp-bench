e60c01fc654c7cbe37da79549ee003cc7c002bb8
Zhongmiao Li <mars.leezm@gmail.com>
2015-05-20 19:30:51 +0100

    Fix another error.

--- script/connectNodesToOtherDCs.sh
+++ script/connectNodesToOtherDCs.sh
@@ -7,16 +7,18 @@ Nodes=`head -$1 ./script/allnodes`
 First=`head -1 ./script/allnodes`
 
 LocalExIP=`ssh -i key ubuntu@$First "curl \"http://169.254.169.254/latest/meta-data/public-ipv4\""`
+echo "Local ex ip is"$LocalExIP
 
 Array=($ExternalIPs)
 for IP in "${Array[@]}"
 do
-    if [ "$IP" = "$LocalExIP" ]
+    if [ "$IP" != "$LocalExIP" ]
     then
         echo here
         NewExtIPs=$NewExtIps" "$IP
     fi
 done
 
+echo "Trying to connect" $Nodes "to" $NewExtIPs
 
-erl -noshell -pa script -setcookie antidote -name setup@localhost -run connectDCs connect  $1 $Nodes  $NewExtIPs
+erl -pa script -setcookie antidote -name setup@localhost -run connectDCs connect  $1 $Nodes  $NewExtIPs
