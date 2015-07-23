556188a62224c6a1bc9ee1299cdb7aac59583795
tcrain <tyler.crain@lip6.fr>
2015-07-23 18:16:26 +0200

    updates for grid5000 scripts

--- script/grid5000start.sh
+++ script/grid5000start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Usage gridjobid benchnode branch"
+    echo "Usage gridjobid benchnode branch dodeploy"
     exit
 fi
 
@@ -11,18 +11,22 @@ fi
 GridJob=$1
 BenchNode=$2
 Branch=$3
+DoDeploy=$4
 Clusters=(`oargridstat $1 | awk '/-->/ { print $1 }'`)
 # Reservations=(`oargridstat $1 | awk '/-->/ { print $3 }'`)
 # JobId=`oargridstat $1 | awk '/Reservation/ { print $3 }' | grep -o '[0-9]*'`
 
-for I in $(seq 0 $((${#Clusters[*]} - 1))); do
-    echo ${Clusters[$I]}
-    ssh ${Clusters[$I]} ~/basho_bench/script/grid5000start-createnodes.sh ${Clusters[$I]} $GridJob &
-    #oargridstat -w -l $JobId | sed '/^$/d' > ~/machines
-    #awk < ~/machines '/'"${Clusters[$I]}"'/ { print $1 }' > ~/machines-tmp
-    #kadeploy3 -f ~/machines-tmp -a ~/antidote_images/mywheezy-x64-base.env -k ~/.ssh/exp_key.pub
-done
-wait
+if [ $DoDeploy -eq 1 ]; then
+    
+    for I in $(seq 0 $((${#Clusters[*]} - 1))); do
+	echo ${Clusters[$I]}
+	ssh ${Clusters[$I]} ~/basho_bench/script/grid5000start-createnodes.sh ${Clusters[$I]} $GridJob &
+	#oargridstat -w -l $JobId | sed '/^$/d' > ~/machines
+	#awk < ~/machines '/'"${Clusters[$I]}"'/ { print $1 }' > ~/machines-tmp
+	#kadeploy3 -f ~/machines-tmp -a ~/antidote_images/mywheezy-x64-base.env -k ~/.ssh/exp_key.pub
+    done
+    wait
+fi
 
 oargridstat -w -l $GridJob | sed '/^$/d' > ~/machines
 awk < ~/machines '!/'"$BenchNode"'/ { print $1 }' > ~/machines-tmp
@@ -40,6 +44,10 @@ for I in $(seq 0 $((${#Clusters[*]} - 1))); do
 done
 echo Nodes per DC: $Size
 echo Number of DCs: $TotalDCs
+
+AllNodes=`cat ~/machines-tmp2`
+Command0="cd ./basho_bench/ && git stash && git fetch && git checkout grid5000 && git pull"
+~/basho_bench/script/parallel_command.sh "$AllNodes" "$Command0"	
 
 scp ~/allnodes root@$BenchNode:~/basho_bench/script/allnodes
 ssh root@$BenchNode /root/basho_bench/script/configMachines.sh $Branch
