336a5d5d897bcd65a7f115a42517021d0425ec6d
Tyler Crain <tyler.crain@lip6.fr>
2015-08-03 22:08:41 +0200

    Updates to grid5000 scripts

--- script/preciseTime.sh
+++ script/preciseTime.sh
@@ -2,6 +2,6 @@
 
 AllNodes=`cat script/allnodes`
 Command3="service ntp stop"
-Command4="/usr/sbin/ntpdate -b ntp1.grid5000.fr"
+Command4="/usr/sbin/ntpdate -b ntp2.grid5000.fr"
 ./script/parallel_command.sh "$AllNodes" "$Command3"	
 ./script/parallel_command.sh "$AllNodes" "$Command4"	
