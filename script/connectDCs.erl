7146122bc318c0ca79f8544c2b52d205c2e7b871
Tyler Crain <tyler.crain@lip6.fr>
2015-08-06 01:43:48 +0200

    Updates to grid5000 scripts

--- script/connectDCs.erl
+++ script/connectDCs.erl
@@ -12,6 +12,8 @@ listenAndConnect(StringNodes) ->
     Nodes = tl(tl(Temp)),
     io:format("Nodes ~w ~n", [Nodes]),
 
+    wait_ready_nodes(Nodes),
+    
     true = erlang:set_cookie(node(), Cookie),
     NumDCs = length(Nodes) div DCPerRing,
     io:format("NumDCs ~w ~n", [NumDCs]),
@@ -22,6 +24,43 @@ listenAndConnect(StringNodes) ->
     DCInfo = addPort(HeadNodes, Ports, []),
     startListeners(DCInfo),
     connect_each(CookieNodes, DCPerRing, 1, DCInfo).
+
+
+wait_ready_nodes([]) ->
+    true;
+wait_ready_nodes([Node|Rest]) ->
+    case check_ready(Node) of
+	true ->
+	    wait_ready_nodes(Rest);
+	false ->
+	    timer:sleep(100),
+	    wait_ready_nodes([Node|Rest])
+    end.
+
+
+check_ready(Node) ->
+    lager:info("Checking if node ~w is ready ~n", [Node]),
+    case rpc:call(Node,clocksi_vnode,check_tables_ready,[]) of
+	true ->
+	    case rpc:call(Node,clocksi_readitem_fsm,check_servers_ready,[]) of
+		true ->
+		    case rpc:call(Node,materializer_vnode,check_tables_ready,[]) of
+			true ->
+			    lager:info("Node ~w is ready! ~n", [Node]),
+			    true;
+			false ->
+			    lager:info("Node ~w is not ready ~n", [Node]),
+			    false
+		    end;
+		false ->
+		    lager:info("Checking if node ~w is ready ~n", [Node]),
+		    false
+	    end;
+	false ->
+	    lager:info("Checking if node ~w is ready ~n", [Node]),
+	    false
+    end.
+
 
 connect(NodeList) ->
     [LocalSize|Rest]=NodeList,
