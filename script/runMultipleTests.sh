47b2d876f17a97214b7275d3e7f20ebb49d9c206
Tyler Crain <tyler.crain@lip6.fr>
2015-08-13 01:27:16 +0200

    info updates

--- script/runMultipleTests.sh
+++ script/runMultipleTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	echo "Usage: numdcs nodesperdc parallelbench numbenchnodesperdc"
+	echo "Usage: numdcs nodesperdc parallelbench numbenchnodesperdc jobid time benchfile"
 	exit
 fi
 
