abd7aca328923476d60dcfb2ad41941d3c1637dd
tcrain <tyler.crain@lip6.fr>
2015-08-17 13:24:50 +0200

    updates

--- script/grid5000start.sh
+++ script/grid5000start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "Usage gridjobid branch dodeploy secondrun benchCount benchParallel benchFile"
+    echo "Usage gridjobid branch dodeploy secondrun computeCount benchCount benchParallel benchFile"
     exit
 fi
 
@@ -12,9 +12,10 @@ GridJob=$1
 Branch=$2
 DoDeploy=$3
 SecondRun=$4
-BenchCount=$5
-BenchParallel=$6
-BenchFile=$7
+ComputeCount=$5
+BenchCount=$6
+BenchParallel=$7
+BenchFile=$8
 Clusters=(`oargridstat $1 | awk '/-->/ { print $1 }'`)
 # Reservations=(`oargridstat $1 | awk '/-->/ { print $3 }'`)
 # JobId=`oargridstat $1 | awk '/Reservation/ { print $3 }' | grep -o '[0-9]*'`
@@ -23,14 +24,16 @@ Clusters=(`oargridstat $1 | awk '/-->/ { print $1 }'`)
 # Filter out the names of the benchmark nodes and the computation nodes
 rm ~/nodelist
 rm ~/benchnodelist
+rm ~/fullnodelist
 CountDC=0
 for I in $(seq 0 $((${#Clusters[*]} - 1))); do
     echo ${Clusters[$I]}
     oargridstat -w -l $GridJob | sed '/^$/d' > ~/machines
     awk < ~/machines '/'"${Clusters[$I]}"'/ { print $1 }' > ~/machines-tmp
     awk < ~/machines-tmp '!seen[$0]++' > ~/machines-tmp2
+    awk < ~/machines-tmp '!seen[$0]++' >> ~/fullnodelist
     head -"$BenchCount" ~/machines-tmp2 >> ~/benchnodelist
-    sed '1,'"$BenchCount"'d' ~/machines-tmp2 >> ~/nodelist
+    sed '1,'"$BenchCount"'d' ~/machines-tmp2 | head -"$ComputeCount" >> ~/nodelist
     CountDC=$(($CountDC + 1))
 done
 
@@ -40,6 +43,7 @@ echo Benchmark nodes: `cat ~/benchnodelist`
 echo
 echo Compute nodes: `cat ~/nodelist`
 echo
+echo Full node list: `cat ~/fullnodelist`
 
 if [ $DoDeploy -eq 1 ]; then
     # Connect to each cluster to deloy the nodes
@@ -60,6 +64,7 @@ fi
 # Change node names to ips
 while read in; do dig +short "$in"; done < ~/nodelist > ~/nodelistip
 while read in; do dig +short "$in"; done < ~/benchnodelist > ~/benchnodelistip
+while read in; do dig +short "$in"; done < ~/fullnodelist > ~/fullnodelistip
 BenchNode=`head -1 ~/benchnodelist`
 
 # Calculate the number of DCs in case there is one that is just benchmark nodes
@@ -83,13 +88,23 @@ mkdir -p logs/"$GridJob"
 if [ $SecondRun -eq 0 ]; then
     # The first run should download and update all code files
     echo The first run
-    AllNodes=`cat ~/benchnodelist`
+    # AllNodes=`cat ~/benchnodelist`
+    # Will compile both antidoe and basho bench on all nodes in case the number changes in a later experiment
+    AllNodes=`cat ~/fullnodelist`
     for I in $(seq 1 $BenchParallel); do
 	echo Checking out 
 	Command0="cd ./basho_bench"$I"/basho_bench/ && git stash && git fetch && git checkout grid5000 && git pull"
 	~/basho_bench/script/parallel_command.sh "$AllNodes" "$Command0" >> logs/"$GridJob"/basho_bench-compile-job"$Time"
     done
 
+    echo Performins configMachines.sh on "$BenchNode"
+    echo First copying the node list to "$BenchNode"
+    echo scp ~/fullnodelistip root@"$BenchNode":/root/basho_bench1/basho_bench/script/allnodes
+    scp ~/fullnodelistip root@"$BenchNode":/root/basho_bench1/basho_bench/script/allnodes
+    echo ssh root@$BenchNode /root/basho_bench1/basho_bench/script/configMachines.sh $Branch
+    ssh -t -o StrictHostKeyChecking=no root@$BenchNode /root/basho_bench1/basho_bench/script/configMachines.sh $Branch $GridJob $Time
+
+    
     # Copy the allnodes file to the benchmark locations
     echo all nodes are `cat ~/nodelistip`
     echo Performing SCPs
@@ -101,10 +116,6 @@ if [ $SecondRun -eq 0 ]; then
 	    scp ~/benchnodelistip root@"$Node":/root/basho_bench"$I"/basho_bench/script/allnodesbench
 	done
     done
-
-    echo Performins configMachines.sh on "$Branch"
-    echo ssh root@$BenchNode /root/basho_bench1/basho_bench/script/configMachines.sh $Branch
-    ssh -t -o StrictHostKeyChecking=no root@$BenchNode /root/basho_bench1/basho_bench/script/configMachines.sh $Branch $GridJob $Time
     
 else
     # Copy the allnodes file to the benchmark locations
