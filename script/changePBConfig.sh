406631983d23efca9657ddb602669502c2b41a29
Tyler Crain <tyler.crain@lip6.fr>
2015-12-15 23:03:10 +0100

    fix bash commad

--- script/changePBConfig.sh
+++ script/changePBConfig.sh
@@ -45,7 +45,7 @@ else
 fi
 
 sed -i '/key_generator/d' $File
-sed -i "3i {key_generator, {dc_bias, $NumDCs, $DcId, $NodesPerDC, 10000}}."
+sed -i "3i {key_generator, {dc_bias, $NumDCs, $DcId, $NodesPerDC, 10000}}." $File
 
 sed -i '/antidote_pb_num_dcs/d' $File 
 sed -i '/antidote_pb_nodes_per_dc/d' $File
