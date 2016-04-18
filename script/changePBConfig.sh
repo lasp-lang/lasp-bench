3355af37c35c67c72eaacb267ea8a6813efa5fe8
tcrain <tyler.crain@lip6.fr>
2016-04-18 18:15:19 +0200

    less keys, pareto

--- script/changePBConfig.sh
+++ script/changePBConfig.sh
@@ -55,8 +55,8 @@ fi
 
 sed -i '/key_generator/d' $File
 #sed -i "3i {key_generator, {dc_bias, $NumDCs, $DcId, $NodesPerDC, 10000}}." $File
-Keys=$(($NodesPerDC * 50000))
-sed -i "3i {key_generator, {uniform_int, $Keys}}." $File
+Keys=$(($NodesPerDC * 10000))
+sed -i "3i {key_generator, {pareto_int, $Keys}}." $File
 
 sed -i '/antidote_pb_num_dcs/d' $File 
 sed -i '/antidote_pb_nodes_per_dc/d' $File
