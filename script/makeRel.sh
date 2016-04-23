fe62095dce523e30dff8911b365d997ecd7ef7c8
root <root@paravance-70.rennes.grid5000.fr>
2016-04-23 03:49:37 +0200

    hard kill beam before compile

--- script/makeRel.sh
+++ script/makeRel.sh
@@ -5,7 +5,8 @@ cd /root/basho_bench1/basho_bench/
 AllNodes=`cat script/allnodes`
 File1="./antidote/rel/vars.config"
 File2="./antidote/rel/files/app.config"
-Command1="./antidote/rel/antidote/bin/antidote stop" 
+#Command1="./antidote/rel/antidote/bin/antidote stop" 
+Command1="pkill beam"
 Command3="cd ./antidote/ && make relnocert"
 
 ./script/parallel_command.sh "$AllNodes" "$Command1"	
