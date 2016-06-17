bbeddfed17420edd9efa668844de6872419473ff
Alejandro Z. Tomsic <aletomsic@gmail.com>
2016-06-17 18:01:16 +0200

    changed update command to read first.

--- script/changePBConfig.sh
+++ script/changePBConfig.sh
@@ -60,9 +60,9 @@ echo "$BenchConfig" >> $File
 sed -i "5i {concurrent, $Thread}." $File
 
 if [ $Type = "counter" ]; then
-    sed -i "6i {operations, [{append, 1}, {read, 1}]}." $File
+    sed -i "6i {operations, [{append, $Writes}, {read, $Reads}]}." $File
 else
-    sed -i "6i {operations, [{update, 1}, {read, 1}]}." $File
+    sed -i "6i {operations, [{update, $Writes}, {read, $Reads}]}." $File
 fi
 #use just transactions.
 #sed -i "6i {operations, [{txn, 1}]}." $File
