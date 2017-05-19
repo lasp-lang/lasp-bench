224077f48692a8bd6eacc60bdb0aad600b8530cd
Gonçalo Tomás <goncalo@goncalotomas.com>
2017-05-19 17:38:57 +0100

    remove unneeded folder

--- deployScript/makeBranch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-Command1="cd ./basho_bench && sudo ./script/configMachines.sh $2"
-Command2="cd ./basho_bench && sudo ./script/makeRel.sh"
-
-./script/parallel_command.sh "$1" "$Command1"
-./script/parallel_command.sh "$1" "$Command2"
